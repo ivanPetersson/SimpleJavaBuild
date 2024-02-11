2ea4614c7c2a44eb4d35df6eaf503b0410935c55
ivanPetersson <ivanpeterssonn@gmail.com>
2024-02-12 00:41:23 +0100

    testing poll scm

--- SimpleJavaBuild.java
+++ SimpleJavaBuild.java
@@ -3,5 +3,6 @@ public class SimpleJavaBuild {
   public static void main(String[] args) {
     System.out.println("Hello World");
     System.out.println("Bla bla bla");
+    System.out.println("Testing build poll SCM");
   }
 }
